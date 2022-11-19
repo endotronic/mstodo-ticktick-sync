af71023a1445bd65ac4e2d1c6c2775484e3f8188
Kevin Wood <endotronic@gmail.com>
2022-11-19 12:45:29 -0800

    Auth in browser

--- importer.py
+++ importer.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from time import sleep
+from threading import Thread
 from os import environ
-from sys import exit
+
+import http.server 
+import socketserver 
+from urllib.parse import urlparse
 
 from pymstodo import ToDoConnection
 from ticktick.oauth2 import OAuth2  # OAuth2 Manager
@@ -9,7 +13,7 @@ from ticktick.api import TickTickClient  # Main Interface
 
 def date_only(dt: datetime) -> datetime:
     return datetime.combine(dt.date(), datetime.min.time())
-
+ 
 
 todo_client_id = environ.get("MICROSOFT_TODO_CLIENT_ID")
 todo_client_secret = environ.get("MICROSOFT_TODO_CLIENT_SECRET")
@@ -22,43 +26,99 @@ ticktick_auth_client = OAuth2(
     get_token_now=False,
 )
 
+token = None
+todo_client = None
 todo_response_url = environ.get("MICROSOFT_TODO_RESPONSE_URL")
 if not todo_response_url:
     auth_url = ToDoConnection.get_auth_url(todo_client_id)
     print("Authorize MS ToDo at " + auth_url)
+else:
+    token = ToDoConnection.get_token(
+        client_id=todo_client_id, 
+        client_secret=todo_client_secret, 
+        redirect_resp=todo_response_url,
+    )
+    todo_client = ToDoConnection(
+        client_id=todo_client_id, 
+        client_secret=todo_client_secret, 
+        token=token,
+    )
 
+ticktick_client = None
 ticktick_response_url = environ.get("TICKTICK_RESPONSE_URL")
 if not ticktick_response_url:
     auth_url = ticktick_auth_client.get_auth_url()
     print("Authorize TickTick at " + auth_url)
 
-if not todo_response_url or not ticktick_response_url:
-    sleep(300)
-    exit(0)
+httpd = None
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        global todo_response_url
+        global ticktick_response_url
+        global todo_client
+        global ticktick_client
 
-token = ToDoConnection.get_token(
-    client_id=todo_client_id, 
-    client_secret=todo_client_secret, 
-    redirect_resp=todo_response_url,
-)
-todo_client = ToDoConnection(
-    client_id=todo_client_id, 
-    client_secret=todo_client_secret, 
-    token=token,
-)
+        url_parts = urlparse(self.path)
+        if url_parts.path == "/login/authorized":
+            try:
+                print("Authorizing with MS ToDo")
+                todo_response_url = "https://localhost/" + self.path # fake it to make oauth2 happy
+                token = ToDoConnection.get_token(
+                    client_id=todo_client_id, 
+                    client_secret=todo_client_secret, 
+                    redirect_resp=todo_response_url,
+                )
+                todo_client = ToDoConnection(
+                    client_id=todo_client_id, 
+                    client_secret=todo_client_secret, 
+                    token=token,
+                )
+            except Exception as e:
+                print(e.message)
+                todo_response_url = None
+        elif url_parts.query.startswith("code"):
+            try:
+                print("Authorizing with TickTick")
+                ticktick_response_url = "https://localhost/" + self.path # fake it to make oauth2 happy
+                ticktick_auth_client.get_access_token(
+                    use_browser=False, 
+                    redirected_url=ticktick_response_url,
+                )
+                ticktick_client = TickTickClient(
+                    environ.get("TICKTICK_USERNAME"),
+                    environ.get("TICKTICK_PASSWORD"),
+                    ticktick_auth_client,
+                )
+            except Exception as e:
+                print(e.message)
+                ticktick_response_url = None
+
+        if not todo_response_url:
+            self.send_response(302)
+            auth_url = ToDoConnection.get_auth_url(todo_client_id)
+            self.send_header('Location', auth_url)
+            print("Redirecting browser to " + auth_url)
+        elif not ticktick_response_url:
+            self.send_response(302)
+            auth_url = ticktick_auth_client.get_auth_url()
+            self.send_header('Location', auth_url)
+            print("Redirecting browser to " + auth_url)
+        else:
+            print("Fully authorized")
+            self.send_response(200, "Done")
+            
+            # Shutdown server in another thread to avoid deadlock
+            Thread(target=httpd.shutdown, daemon=True).start()
+       
+        self.end_headers()
+
+if not todo_response_url or not ticktick_response_url:
+    with socketserver.TCPServer(("", 8080), MyHttpRequestHandler) as httpd:
+        print("Enabled HTTP server on port 8080 to aid authentication")
+        httpd.serve_forever()
 
 lists = todo_client.get_lists()
 task_list = lists[0]
-
-ticktick_auth_client.get_access_token(
-    use_browser=False, 
-    redirected_url=ticktick_response_url,
-)
-ticktick_client = TickTickClient(
-    environ.get("TICKTICK_USERNAME"),
-    environ.get("TICKTICK_PASSWORD"),
-    ticktick_auth_client,
-)
 
 print("Starting...")
 while True:
